e54c785c061ca0642665f4e043784f97b20bbe09
Diego Milan <diego.milan@duedil.com>
2017-03-29 19:51:51 +0100

    Install oh my zsh for vagrant

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,5 +9,6 @@ bash "zsh default shell for vagrant" do
     command <<-EOF
     sed -i '/^vagrant/s_/bin/bash_/usr/bin/zsh_' /etc/passwd
     echo 'ZSH_THEME="robbyrussell"' | tee -a /home/vagrant/.zshrc
+    su vagrant -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh'
     EOF
 end
