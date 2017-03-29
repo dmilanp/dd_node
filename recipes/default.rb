3f6945bf6131261266b157c65236fc1630a81c99
Diego Milan <diego.milan@duedil.com>
2017-03-29 19:14:13 +0100

    Make zsh default shell for vagrant

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,10 @@ apt_update "update packages" do
 end
 
 apt_package "zsh"
+
+bash "zsh default shell for vagrant" do
+    command <<-EOF
+    sed '/^vagrant/s_/bin/bash_/usr/bin/zsh_' /etc/passwd
+    echo 'ZSH_THEME="robbyrussell"' | tee -a /home/vagrant/.zshrc
+    EOF
+end
