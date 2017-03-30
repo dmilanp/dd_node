c7873ba90243160bf1573f775814f7d45ded6c98
Diego Milan <diego.milan@duedil.com>
2017-03-30 13:24:44 +0100

    Add zsh for vagrant and root

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,12 +3,22 @@ apt_update "update packages" do
     action  :update
 end
 
-apt_package "zsh"
-
-bash "zsh default shell for vagrant" do
-    command <<-EOF
-    sed -i '/^vagrant/s_/bin/bash_/usr/bin/zsh_' /etc/passwd
-    echo 'ZSH_THEME="robbyrussell"' | tee -a /home/vagrant/.zshrc
-    su vagrant -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh'
-    EOF
+%w{zsh tree}.each do |pkg|
+    apt_package pkg
 end
+
+%w{vagrant root}.each do |usr|
+    bash "zsh default shell for #{usr}" do
+        code <<-EOF
+        sed -i '/^#{usr}/s_/bin/bash_/usr/bin/zsh_' /etc/passwd
+        su #{usr} -c 'wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh'
+        EOF
+    end
+end
+
+bash "zsh theme for vagrant and root" do
+    code "sed -i 's/robbyrussell/sonicradish/' /root/.zshrc && \
+          sed -i 's/robbyrussell/sonicradish/' /home/vagrant/.zshrc"
+end
+
+
